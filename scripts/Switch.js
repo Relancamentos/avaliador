c74682dfe64ef3b8fb57e242ccbcd47d79b5e63b
Relancamentos <135891430+Relancamentos@users.noreply.github.com>
2023-08-21 12:02:24 -0300

    Update Switch.js

--- scripts/Switch.js
+++ scripts/Switch.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Aguarde o DOM estar pronto
 
   // Defina o tempo em segundos após o qual a classe "esconder" será removida
-  var segundos = 50;
+  var segundos = 500;
 
   setTimeout(function() {
     // Coloque aqui o código para remover a classe "esconder"
