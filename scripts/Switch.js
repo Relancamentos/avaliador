ddd7c3fc327bd47da24db2de51e0bcc7ce2d1006
Relancamentos <135891430+Relancamentos@users.noreply.github.com>
2023-08-21 11:46:38 -0300

    Create Switch.js

---
+++ scripts/Switch.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Aguarde o DOM estar pronto
+
+  // Defina o tempo em segundos após o qual a classe "esconder" será removida
+  var segundos = 10;
+
+  setTimeout(function() {
+    // Coloque aqui o código para remover a classe "esconder"
+    var botao = document.getElementById("video-button-acesso");
+    botao.classList.remove("esconder");
+  }, segundos * 1000); // Converta segundos em milissegundos
+});
